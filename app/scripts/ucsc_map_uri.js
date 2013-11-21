d96ac9a4db06abf00d292fba643594315f70eeba
Peter McMillan <peterm95018@gmail.com>
2013-11-21 13:53:58 -0800

    added ucsc_map_uri.js, modified layouts on map,cafe_list,index

---
+++ app/scripts/ucsc_map_uri.js
@@ -0,0 +1,19 @@
+var parser = document.createElement('a');
+
+var url = document.URL;
+
+parser.href = url;
+
+parser.protocol; // => "http:"
+parser.hostname; // => "example.com"
+parser.port;     // => "3000"
+parser.pathname; // => "/pathname/"
+parser.search;   // => "?search=test"
+parser.hash;     // => "#hash"
+parser.host;     // => "example.com:3000"
+
+
+var query = parser.hash; // returns "#17/36.999998/-122.05443"
+var pairs = query.split("/"); // Split at "/", ["#15", "36.9991", "-122.0549"]
+var lat = pairs[1];
+var lng = pairs[2];
