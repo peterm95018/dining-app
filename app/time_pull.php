262c92ca3710f4cab9c3b5fdc82b11740e416a2f
Aidenator <aidenator@Aiden-12.04>
2014-08-04 16:51:02 -0700

    Successfully connects to MySQL server. Prints rudimentary info on time_tracking.html if you select a Dining Hall.

--- app/time_pull.php
+++ app/time_pull.php
@@ -1,4 +1,36 @@
 <?php
+//Allow full error reporting
+ini_set('display_errors', '1');
+ini_set('error_reporting', E_ALL);
 
+//Get loc # from URL
+$location_number = $_POST['location'];
+
+$serverName = 'localhost';
+$database = 'dininghours';
+$username = 'resnet';
+$password = '';
+$options = array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+); 
+
+try {
+      $conn = new PDO( "mysql:host=$serverName;dbname=$database", $username, $password);
+      $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+   }
+
+   catch( PDOException $e ) {
+      print_r($e);
+      die( "Error connecting to SQL Server" );
+   }
+$query = "SELECT * FROM hours";
+
+$stmt = $conn->query( $query );
+
+while ( $row = $stmt->fetch( PDO::FETCH_ASSOC ) ){
+	$rows[] = $row;
+}
+
+print json_encode($rows);
 
 ?>
